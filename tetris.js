a660985342be5a3d6f0e2caf0b3c55e2dc7d1f01
Arin <arinmsn@gmail.com>
2019-07-29 08:50:06 -0700

    When T was rotated, it would move outside view of the arena. This was fixed. *q* and *w*  keys rotate the pieace.

--- tetris.js
+++ tetris.js
@@ -74,6 +74,49 @@ function playerDrop() {
     dropCounter = 0;
 }
 
+// User cannot exit the arena (left or right)
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
+    }
+}
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    // Check direction
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -99,11 +142,15 @@ const player = {
 // Key controls
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        player.pos.x--; // move to left
+        playerMove(-1); // move to left
     } else if (event.keyCode === 39) {
-        player.pos.x++; // move to right
+        playerMove(1); // move to right
     } else if (event.keyCode === 40) {
         playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
     /* Switch method works as well!
     switch (event.keyCode) {
