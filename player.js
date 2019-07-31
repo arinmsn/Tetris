f04ca57cc4f6065e49f99c19ae293ee63654e8d1
arinmsn <arinmsn@gmail.com>
2019-07-30 21:19:20 -0700

    Separated player into its own file (class). So far, separated playerMove and playerRotate from the main tetris file.

---
+++ player.js
@@ -0,0 +1,33 @@
+class Player 
+{
+    constructor() {
+        this.pos = {x: 0, y:0};
+        this.matrix = null;
+        this.score = 0;
+    }
+
+
+    // User cannot exit the arena (left or right)
+    move(dir) {
+        this.pos.x += dir;
+        if (collide(arena, this)) {
+            this.pos.x -= dir;
+        }
+    }
+
+    // When pieace is rotated, it will remain in arena 
+    rotate(dir) {
+        const pos = this.pos.x;
+        let offset = 1;
+        rotate(this.matrix, dir);
+        while(collide(arena, this)) {
+            this.pos.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1));
+            if (offset > this.matrix[0].length) {
+                rotate(this.matrix, -dir);
+                this.pos.x = pos;
+                return;
+            }
+        }
+    }
+}
--- tetris.js
+++ tetris.js
@@ -136,13 +136,6 @@ function playerDrop() {
     dropCounter = 0;
 }
 
-// User cannot exit the arena (left or right)
-function playerMove(offset) {
-    player.pos.x += offset;
-    if (collide(arena, player)) {
-        player.pos.x -= offset;
-    }
-}
 
 function playerReset() {
     const pieces = 'ILJOTSZ';
@@ -153,23 +146,6 @@ function playerReset() {
         arena.forEach(row => row.fill(0));
         player.score = 0;
         updateScore();
-    }
-}
-
-
-// When pieace is rotated, it will remain in arena 
-function playerRotate(dir) {
-    const pos = player.pos.x;
-    let offset = 1;
-    rotate(player.matrix, dir);
-    while(collide(arena, player)) {
-        player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length) {
-            rotate(player.matrix, -dir);
-            player.pos.x = pos;
-            return;
-        }
     }
 }
 
@@ -225,24 +201,20 @@ const colors = [
 
 const arena = createMatrix(12, 20);
 
-const player = {
-    pos: {x: 0, y:0},
-    matrix: null,
-    score: 0
-}
+const player = new Player;
 
 // Key controls
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        playerMove(-1); // move to left
+        player.move(-1); // move to left
     } else if (event.keyCode === 39) {
-        playerMove(1); // move to right
+        player.move(1); // move to right
     } else if (event.keyCode === 40) {
         playerDrop();
     } else if (event.keyCode === 81) {
-        playerRotate(-1);
+        player.rotate(-1);
     } else if (event.keyCode === 87) {
-        playerRotate(1);
+        player.rotate(1);
     }
 });
 
