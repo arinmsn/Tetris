90154c5e3bf9646d82ff8273963f5fa2db829fc0
arinmsn <arinmsn@gmail.com>
2019-08-02 21:33:25 -0700

    Minor changes made to arena.js

--- arena.js
+++ arena.js
@@ -40,6 +40,7 @@ class Arena {
 
     sweep() {
         let rowCount = 1;
+        let score = 0;
         outer: for (let y = this.matrix.length - 1; y > 0; --y) {
             for (let x = 0; x < this.matrix[y].length; ++x) {
                 if (this.matrix[y][x] === 0) { // Not fully populated
@@ -49,8 +50,9 @@ class Arena {
             const row = this.matrix.splice(y, 1)[0].fill(0);
             this.matrix.unshift(row);
             ++y;
-            player.score += rowCount * 10;
+            score += rowCount * 10;
             rowCount *= 2; 
         }
+        return score;
     }
 }
